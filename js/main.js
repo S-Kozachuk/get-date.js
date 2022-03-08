72f211d8a679e6fc9d9c07b8220c554c52a768e9
S-Kozachuk <kozachuk-dev@yandex.ru>
2022-03-08 13:56:43 +0300

    PT_1/Added comments to JS file

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
+// Получаю в константу кнопку (стрелку)
 const scrollBtn = document.querySelector('.show__btn');
 window.onscroll = () => {
+	// Появление кнопки при вертикальной прокрутке вниз (более чем на 400 px)
 	if(window.scrollY > 400) {
 		scrollBtn.classList.remove('show__btn-hide');
 	}
+	// Исчезновение кнопки при вертикальной прокрутке вверх (менее 400 px)
 	else if (window.scrollY < 400) {
 		scrollBtn.classList.add('show__btn-hide');
 	}
 };
 
+// Прокрутка вверх (по координатам) по нажатию кнопки (стрелки)
 scrollBtn.onclick = () => {
 	window.scrollTo(0, 0);
 };

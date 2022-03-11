788f4b83047fb0c535d5c2552a8f4b1215b3225e
S-Kozachuk <kozachuk-dev@yandex.ru>
2022-03-11 13:29:51 +0300

    PT_2/Comment other code.

--- js/date.js
+++ js/date.js
@@ -43,73 +43,81 @@ console.log(getDayInfo(date));
 
 // Урок по объекту Data в JS от Александра Лущенко
 // Получение локального (текщего) времени (установленного в ОС).
+/*
 const d1 = new Date ();
 console.log(d1);
-
+*/
 // Время (дата), выраженное в миллисекундах
 /*
 Работать с временем, выраженным в мс горздо удобнее с точки зрения 
 математики. Не забывать добавлять 1000 (*1000).
 */
+/*
 const d2 = new Date (1646812749000);
 console.log(d2); 
-
+*/
 // Можно задать время явно
 /*
 Указать как аргумент для обекта Date в формате:
 (год, месяц, день, часы, минуты).
 Месяца начинаются с 0 (январь).
 */ 
+/*
 const d3 = new Date(2021, 8, 6);
 console.log(d3);
+*/
 // Получение объекта времени из строки (парсинг)
-const d4 = new Date('2019 Sep 6');
+//const d4 = new Date('2019 Sep 6');
 // ввывод типа данных: объект
+/*
 console.log(typeof d4)
 console.log(d4);
+*/
 
 // Перевод строки в объект
+/*
 const d5 = new Date ('Mon Sep 06 2021 11:19:25 GMT+0300');
 console.log(d5);
 document.querySelector('.out-1').innerHTML = d5;
+*/
 // Среднее время по меридиану (перевод объекта data  в строку по UTC, -3 часа)	
-document.querySelector('.out-2').innerHTML = d5.toUTCString();
+//document.querySelector('.out-2').innerHTML = d5.toUTCString();
 // Вывод только даты без времени и указания часового пояса
-document.querySelector('.out-3').innerHTML = d5.toDateString();
+//document.querySelector('.out-3').innerHTML = d5.toDateString();
 // Вывод времени по стандарту ISO в виде строки
-document.querySelector('.out-4').innerHTML = d5.toISOString();
+//document.querySelector('.out-4').innerHTML = d5.toISOString();
 
 
 // Функция для форматирования даты в читабельный вид
 // Задача научиться получать из объекта date данные в нужном формате
-const d20 = new Date();
+//const d20 = new Date();
 // Методы для извлечения нужных блоков данных
 // Получение значения текущего года в полном формате
-console.log("Год, полностью:", d20.getFullYear());
+//console.log("Год, полностью:", d20.getFullYear());
 // Получение значения текущего месяца (с 0 до 11)
-console.log("Месяц, с Янв(0):", d20.getMonth());
+//console.log("Месяц, с Янв(0):", d20.getMonth());
 // Получение значения текущего дня (с 1 до 31)
 /*
 При попытке ввести значение, превышающее макс., например 32
 - день будет пересён на след. мес. Месяц будет пересчитан.
 */
-console.log("День:", d20.getDate());
+//console.log("День:", d20.getDate());
 // Получение значения текущего дня недели начиная с Вс по Сб (с 0 до 6)
-console.log("День недели, с Вс(0):", d20.getDay());
+//console.log("День недели, с Вс(0):", d20.getDay());
 // Получение значения текущего времени в часах (с 0 до 24, с учётом часовой зоны)
-console.log("Часы:", d20.getHours());
+//console.log("Часы:", d20.getHours());
 // Получение значения текущего времени в минутах (от 0 до 59)
-console.log("Минуты:", d20.getMinutes());
+//console.log("Минуты:", d20.getMinutes());
 // Получение значения текущего времени в секундах (от 0 до 59)
-console.log("Секунды:", d20.getSeconds());
+//console.log("Секунды:", d20.getSeconds());
 // Получение значения текущего времени в эпохе Unix
-console.log("Unix-time:", d20.getTime());
+//console.log("Unix-time:", d20.getTime());
 // Получение значения текущего времени в эпохе Unix (выаолняется непосредственно в момент запуска)
-console.log("Unix-time (через метод .now):", Date.now());
+//console.log("Unix-time (через метод .now):", Date.now());
 
 
 // Function getUserTime
-// Функция, добавляющая ведущий ноль (перобразует число в строку)
+// Функция addLeadingZero, добавляющая ведущий ноль (перобразует число в строку)
 function addLeadingZero(z) {
 	return (z < 10) ? '0' + z : z;
 }
@@ -118,8 +126,6 @@ function addLeadingZero(z) {
 const daysName = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
 // Массив для конвертации номера месяца в его название (январь - декабрь)
 const monthName = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
-// Массив для определения номера недели в зависимости от указнного дня
-const weekNumber = [1, 2, 3, 4]
 
 // Функция, определяющая номер недели по указанному дню
 function weekId (w) {
@@ -147,7 +153,9 @@ function getUserTime(t) {
 	// Извлекаем год (Y = Year)
 	let Y = t.getFullYear();
 	// Извлекаем месяц (М = Month) +1 (корректировка значения) т.к. значения выводятся c 0 до 11
-	let M = addLeadingZero(t.getMonth() +1);
+	let M = t.getMonth();
+	// addLeadingZero
+	
 	// if (M < 10) M = '0' + M; - более простой, но меннее лакончичный вариант добавлениея ведущего 0
 	// Извлекаем номер дня в рамках месяца
 	let D = addLeadingZero(t.getDate());
@@ -165,14 +173,14 @@ function getUserTime(t) {
 	// Извлекаем время в формате минут
 	let m = addLeadingZero(t.getMinutes());
 	// Извлекаем название месяца (Январь - Февраль)
-	let d = monthName[t.getMonth()];
+	let d = monthName[M];
 	let w = weekId(t.getDate());
 	console.log(Y, M, D, z, h, m, d, w);
 	// Возврат отформатированных данных (значений) в функцию с помощью оператора return
-	return `${h}:${m} | ${D}.${M}.${Y} | ${z} | ${d} | ${w} неделя`
+	return `${h}:${m} | ${D}.${M+1}.${Y} | ${z} | ${d} | ${w} неделя`
 }
 
-console.log(getUserTime(new Date(1991, 5, 22)));
+console.log(getUserTime(new Date('2022, 5, 31')));
 
 /*
 function range(a,b){

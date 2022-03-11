22f4d4ff997a80dee6b7a417b21c891d90b376aa
S-Kozachuk <kozachuk-dev@yandex.ru>
2022-03-11 15:10:34 +0300

    PW/Copy execute code from date.js

--- js/parse-week.js
+++ js/parse-week.js
@@ -1,18 +1,19 @@
+// Массив для конвертации номера дня недели в название (понедельни - воскресенье)
+const daysName = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+// Массив для конвертации номера месяца в его название (январь - декабрь)
+const monthName = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
+
+
 function getUserTime(t) {
 	let Y = t.getFullYear();
 	let M = t.getMonth();
-	let D = addLeadingZero(t.getDate());
+	let D = t.getDate();
 	let z = daysName[t.getDay()];
-	// Извлекаем время в формате часов
-	let h = addLeadingZero(t.getHours());
-	// Извлекаем время в формате минут
-	let m = addLeadingZero(t.getMinutes());
-	// Извлекаем название месяца (Январь - Февраль)
+	let h = t.getHours();
+	let m = t.getMinutes();
 	let d = monthName[M];
-	let w = weekId(t.getDate());
-	console.log(Y, M, D, z, h, m, d, w);
-	// Возврат отформатированных данных (значений) в функцию с помощью оператора return
-	return `${h}:${m} | ${D}.${M+1}.${Y} | ${z} | ${d} | ${w} неделя`
+	console.log(Y, M, D, z, h, m, d);
+	return `${h}:${m} | ${D}.${M+1}.${Y} | ${z} | ${d} | неделя`
 }
 
 console.log(getUserTime(new Date('2022, 5, 31')));
